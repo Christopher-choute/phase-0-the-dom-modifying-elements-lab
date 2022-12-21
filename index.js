932e983f2fe9ae9c80a432b93ae1a06cb61d5efe
Chris Choute <christopherchoute@gmail.com>
2022-12-20 22:45:20 -0500

    commit tree 0d213a65df11b94ce6a30f746a36d572e55e0257

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+// Write your code here
+main.remove()
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory'
+
+const evenNewerHeader = document.getElementById('victory');
+newHeader.innerHTML = 'Chris is the champion';
