3076aad0d08b9ffb2696e6e208a9005c2b33a5eb
Chris Choute <christopherchoute@gmail.com>
2023-01-13 09:36:54 -0500

    some changes

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+// Write your code here
+main.remove()
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory'
+newHeader.textContent = "Chris is the champion"
+
+const evenNewerHeader = document.getElementById('victory');
+evenNewerHeader.innerHTML = 'Chris is the champion';
+
+console.log(evenNewerHeader)
